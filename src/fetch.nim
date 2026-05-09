5d38e93291ae0fc9e5db93f46deea51d2d116441
mostypc123 <mostypc123@redroselinux.org>
2026-05-09 16:43:16 +0200

    feat: add fetching and all the basic stuff

--- src/fetch.nim
+++ src/fetch.nim
@@ -1,2 +1,83 @@
-proc fetchLatest*() =
-    echo "h"
+import osproc
+import strutils
+
+import format_article
+
+proc fetchLatest*(): int =
+  var p = startProcess(
+    "curl",
+    args = [
+      "-s", "-L", "-o", "/tmp/news-reader-latest",
+      "https://github.com/redroselinux/news-reader/" &
+      "raw/refs/heads/master/news/latest"
+    ],
+    options = {poUsePath}
+  )
+
+  if p.waitForExit() != 0:
+    p.close()
+    echo "\e[91m*\e[0m Failed to fetch latest news"
+    return 1
+  p.close()
+
+  var latest: string
+  try:
+    latest = readFile("/tmp/news-reader-latest").strip()
+  except:
+    echo "\e[91m*\e[0m Failed to read latest news"
+    return 1
+
+  # latest is simply a url; fetch the url we just got
+  p = startProcess(
+    "curl",
+    args = ["-s", "-L", "-o", "/tmp/news-reader-latest", latest],
+    options = {poUsePath}
+  )
+
+  if p.waitForExit() != 0:
+    p.close()
+    echo "\e[91m*\e[0m Failed to fetch latest news"
+    return 1
+  p.close()
+
+  try:
+    latest = readFile("/tmp/news-reader-latest")
+  except:
+    echo "\e[91m*\e[0m Failed to read latest news"
+    return 1
+  if latest.strip.startsWith("<!DOCTYPE html>"):
+    echo "\e[91m*\e[0m Failed to fetch latest"
+    return 1
+  
+  echo formatArticle(latest)
+  return 0
+
+proc fetchSpecific*(date: string): int =
+  var p = startProcess(
+    "curl",
+    args = [
+      "-s", "-L", "-o", "/tmp/news-reader-latest",
+      "https://github.com/redroselinux/news-reader/" &
+      "raw/refs/heads/master/news/" & date
+    ],
+    options = {poUsePath}
+  )
+
+  if p.waitForExit() != 0:
+    p.close()
+    echo "\e[91m*\e[0m Failed to fetch article " & date
+    return 1
+  p.close()
+
+  var latest: string
+  try:
+    latest = readFile("/tmp/news-reader-latest")
+  except:
+    echo "\e[91m*\e[0m Failed to read " & date
+    return 1
+  if latest.strip.startsWith("<!DOCTYPE html>"):
+    echo "\e[91m*\e[0m Failed to fetch " & date
+    return 1
+
+  echo formatArticle(latest)
+  return 0
--- src/news_reader.nim
+++ src/news_reader.nim
@@ -7,7 +7,7 @@ when isMainModule:
   let argc = len(argv)
 
   if argc == 0:
-    fetchLatest()
+    quit fetchLatest()
   else:
     if argv[0] == "help":
       echo "news-reader v0.1"
@@ -16,18 +16,24 @@ when isMainModule:
       echo "usage: news-reader [command] [args]"
       echo ""
       echo "commands:"
-      echo "  (no args)           fetch and read latest"
-      echo "  read <date>         fetch and read a specific article"
-      echo "  mark read <id>      mark as read (latest or <date>)"
-      echo "  mark unread <id>    mark as unread (latest or <date>)"
-      echo "  help                show this help message"
+      echo "  (no args)      fetch and read latest"
+      echo "  read <date>    fetch and read a specific article"
+      echo "  help           show this help message"
       echo ""
       echo "examples:"
-      echo "  news-reader read 2026(09.04)"
+      echo "  news-reader read 2026\\(9.5\\)"
       echo "  news-reader mark read latest"
+      quit 0
     elif argv[0] == "read":
       if argc == 1:
         echo "\e[91m*\e[0m You must specify the article to read"
         echo "\e[93m*\e[0m Run 'news-reader help'"
+        quit 1
+      
+      if argv[1] == "latest":
+        quit fetchLatest()
+      else:
+        quit fetchSpecific(argv[1])
     else:
       echo "\e[91m*\e[0m Unknown command"
+      quit 1
